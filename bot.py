f15da59aac3d00c34c59479642d97d447ba9e695
ChaozzS <andriy11951@gmail.com>
2025-05-14 22:14:56 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,42 @@
+import os
+import json
+import threading
+from flask import Flask, send_from_directory
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+app_web = Flask(__name__)
+
+@app_web.route("/")
+def menu():
+    return send_from_directory("static", "menu.static")
+
+def run_flask():
+    app_web.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = os.getenv("WEBAPP_URL")
+    keyboard = [
+        [InlineKeyboardButton("📋 Відкрити меню", web_app=WebAppInfo(url=f"{url}"))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Натисни кнопку нижче, щоб відкрити меню:", reply_markup=reply_markup)
+
+async def handle_webapp_data(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        data = json.loads(update.message.web_app_data.data)
+        dish = data.get("dish")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"🍽️ Ви обрали: {dish}")
+    except Exception as e:
+        print("❌ Помилка:", e)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_flask).start()
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, handle_webapp_data))
+    app.run_polling()
